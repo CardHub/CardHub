0c717c1c17fcd89338d2bcdd4a9c79045b7f9cc3
WYHNUS <wangyanhao1993@163.com>
2016-09-14 03:44:38 +0800

    dummy ui for createCard page

--- frontend/app/scripts/controllers/createDeck.js
+++ frontend/app/scripts/controllers/createDeck.js
@@ -8,8 +8,11 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('CreateDeckCtrl', function ($scope) {
+  .controller('CreateDeckCtrl', function ($scope, $state) {
     $scope.create = function(deck) {
       console.log(deck);
+      // add deck to backend, then create card in the callback
+      var response = {"deckId" : "dummyDeck123"};
+      $state.go('main.createCard', {deckId: response.deckId});
     };
   });
