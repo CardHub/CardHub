0d4cc94434be2d31bf8a0b6f61c199b3840428e7
WYHNUS <wangyanhao1993@163.com>
2016-09-16 21:35:53 +0800

    remove unnecessary files (seperated states which have been replaced by mdDialog)

--- frontend/app/scripts/controllers/card.js
+++ frontend/app/scripts/controllers/card.js
@@ -8,31 +8,66 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('CardCtrl', function ($scope, $stateParams, $state, UserAuth, apiHelper) {
+  .controller('CardCtrl', function ($scope, $stateParams, $state, $mdDialog, UserAuth, apiHelper) {
     $scope.deckId = $stateParams.deckId;
     $scope.cardId = $stateParams.cardId;
 
     $scope.card = {};
-    // $scope.deck = [];
+    $scope.deck = [];
     $scope.isOwner = false;
-    apiHelper.deck.show($scope.deckId).then(function(res) {
-      console.log(res.data);
-      $scope.deck = res.data;
-      for (var i = 0; i < $scope.deck.Cards.length; i++) {
-        if ($scope.deck.Cards[i].id == $scope.cardId) {
-          $scope.card = $scope.deck.Cards[i];
-          break;
-        }
-      }
-      // check if current user is the owner
-      $scope.isOwner = (UserAuth.getCurrentUser().id === $scope.deck.UserId);
-    }).catch(function(err) {
-      console.log(err);
-    });
-
     $scope.isCardFront = true;
+    function getCardInDeck() {
+      apiHelper.deck.show($scope.deckId).then(function(res) {
+        console.log(res.data);
+        $scope.deck = res.data;
+        for (var i = 0; i < $scope.deck.Cards.length; i++) {
+          if ($scope.deck.Cards[i].id == $scope.cardId) {
+            $scope.card = $scope.deck.Cards[i];
+            break;
+          }
+        }
+        // check if current user is the owner
+        $scope.isOwner = (UserAuth.getCurrentUser().id === $scope.deck.UserId);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }
+    getCardInDeck();
 
-    $scope.editCard = function() {
-      $state.go('main.editCard', {deckId: $scope.deckId, cardId: $scope.cardId});
+    $scope.showEditCardDialog = function(event) {
+      $mdDialog.show({
+        controller: EditCardCtrl,
+        templateUrl: 'views/editCard.html',
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose: true,
+        fullscreen: true,
+        locals: {
+          // deckName: $scope.deck.name
+        }
+      })
+      .then(function(newCard){
+        apiHelper.card.update($scope.deckId, $scope.cardId, newCard).then(function(res) {
+          console.log(res.data);
+          getCardInDeck();
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+      }, function() {
+      });
     };
+
+    function EditCardCtrl($scope, $mdDialog) {
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+      $scope.update = function(card) {
+        var newCard = {
+          front: card.front,
+          back: card.back
+        };
+        $mdDialog.hide(newCard);
+      };
+    }
   });
